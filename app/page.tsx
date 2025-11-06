fe77180551b8c662a1b0bc66473e69ce88367ef3
DhanushkaSandaruwan <dhanushkasandaruwan350@gmail.com>
2025-11-06 21:32:37 +0530

    refactor: remove companies from render list

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,6 @@ export default function Home() {
       <main className="flex min-h-screen w-full flex-col items-center justify-between bg-white dark:bg-black sm:items-start">
         <Navm />
         <Hero />
-        <Companies />
         <Bento />
         <Features />
         <CtaS />
